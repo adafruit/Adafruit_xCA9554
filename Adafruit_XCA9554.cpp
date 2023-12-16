6e495f7ee0b4e56c794dc8e394f54d89e2a239a5
ladyada <limor@ladyada.net>
2023-12-16 15:15:17 -0500

    doxyclang

--- Adafruit_XCA9554.cpp
+++ Adafruit_XCA9554.cpp
@@ -10,14 +10,16 @@
  * Adafruit dev and breakout boards that use TCA9554 or PCA9554
  *
  * This library allows you to communicate with the TCA/PCA9554 I/O expander chip
- * using I2C. The chip extends the number of I/O pins available for use in 
- * your projects. It is capable of interfacing with both 3.3V and 5V logic levels.
+ * using I2C. The chip extends the number of I/O pins available for use in
+ * your projects. It is capable of interfacing with both 3.3V and 5V logic
+ * levels.
  *
  * @section dependencies Dependencies
  *
- * This library depends on the <a href="https://github.com/adafruit/Adafruit_BusIO">
- * Adafruit BusIO library</a> for low-level communication, and the standard Wire library
- * for I2C communication.
+ * This library depends on the <a
+ * href="https://github.com/adafruit/Adafruit_BusIO"> Adafruit BusIO library</a>
+ * for low-level communication, and the standard Wire library for I2C
+ * communication.
  *
  * @section author Author
  *
@@ -33,15 +35,13 @@
 /**
  * @brief Construct a new Adafruit_XCA9554::Adafruit_XCA9554 object.
  */
-Adafruit_XCA9554::Adafruit_XCA9554() : 
-  i2c_dev(nullptr), input_port_reg(nullptr),
-  output_port_reg(nullptr), polarity_inversion_reg(nullptr),
-  config_reg(nullptr) {
-}
+Adafruit_XCA9554::Adafruit_XCA9554()
+    : i2c_dev(nullptr), input_port_reg(nullptr), output_port_reg(nullptr),
+      polarity_inversion_reg(nullptr), config_reg(nullptr) {}
 
 /**
  * @brief Initialize the XCA9554 chip.
- * 
+ *
  * @param i2c_addr I2C address of the XCA9554 chip.
  * @param wire The I2C interface to use, defaults to Wire.
  * @return true if initialization is successful, false otherwise.
@@ -54,25 +54,27 @@ bool Adafruit_XCA9554::begin(uint8_t i2c_addr, TwoWire *wire) {
   if (!i2c_dev->begin()) {
     return false;
   }
-  
+
   // Initialize register objects
   input_port_reg = new Adafruit_BusIO_Register(i2c_dev, XCA9554_REG_INPUT_PORT);
-  output_port_reg = new Adafruit_BusIO_Register(i2c_dev, XCA9554_REG_OUTPUT_PORT);
-  polarity_inversion_reg = new Adafruit_BusIO_Register(i2c_dev, XCA9554_REG_POLARITY_INVERT);
+  output_port_reg =
+      new Adafruit_BusIO_Register(i2c_dev, XCA9554_REG_OUTPUT_PORT);
+  polarity_inversion_reg =
+      new Adafruit_BusIO_Register(i2c_dev, XCA9554_REG_POLARITY_INVERT);
   config_reg = new Adafruit_BusIO_Register(i2c_dev, XCA9554_REG_CONFIG);
-  
+
   // Configure all pins as inputs by default
   uint8_t configValue = 0xFF; // All pins as input
   if (!config_reg->write(configValue)) {
     return false;
   }
-  
+
   return true;
 }
 
 /**
  * @brief Set the mode of a pin (INPUT/OUTPUT).
- * 
+ *
  * @param pin The pin number (0-7) to set.
  * @param mode The mode to set the pin (INPUT/OUTPUT).
  * @returns Whether we successfully wrote the bit to the register
@@ -82,10 +84,11 @@ bool Adafruit_XCA9554::pinMode(uint8_t pin, uint8_t mode) {
   if (!i2c_dev || !config_reg || pin > 7 || (mode != INPUT && mode != OUTPUT)) {
     return false;
   }
-  
+
   // Create a RegisterBits object for the specific pin
-  Adafruit_BusIO_RegisterBits pinModeBit = Adafruit_BusIO_RegisterBits(config_reg, 1, pin);
-  
+  Adafruit_BusIO_RegisterBits pinModeBit =
+      Adafruit_BusIO_RegisterBits(config_reg, 1, pin);
+
   if (mode == OUTPUT) {
     return pinModeBit.write(0); // Set bit to 0 for output
   } else {
@@ -93,10 +96,9 @@ bool Adafruit_XCA9554::pinMode(uint8_t pin, uint8_t mode) {
   }
 }
 
-
 /**
  * @brief Set the state of a pin (HIGH/LOW).
- * 
+ *
  * @param pin The pin number (0-7) to set.
  * @param val The state to set the pin (true for HIGH, false for LOW).
  * @returns Whether we successfully wrote the bit to the register.
@@ -105,30 +107,32 @@ bool Adafruit_XCA9554::digitalWrite(uint8_t pin, bool val) {
   if (!i2c_dev || !output_port_reg || pin > 7) {
     return false; // Validate parameters
   }
-  
+
   // Create a RegisterBits object for the specific pin
-  Adafruit_BusIO_RegisterBits outputBit = Adafruit_BusIO_RegisterBits(output_port_reg, 1, pin);
-  
+  Adafruit_BusIO_RegisterBits outputBit =
+      Adafruit_BusIO_RegisterBits(output_port_reg, 1, pin);
+
   return outputBit.write(val ? 1 : 0); // Set the pin state
 }
 
-
 /**
  * @brief Read the state of a pin (HIGH/LOW).
- * 
+ *
  * @param pin The pin number (0-7) to read.
- * @returns The state of the pin (true for HIGH, false for LOW), or false if an error occurs.
+ * @returns The state of the pin (true for HIGH, false for LOW), or false if an
+ * error occurs.
  */
 bool Adafruit_XCA9554::digitalRead(uint8_t pin) {
   if (!i2c_dev || !input_port_reg || pin > 7) {
     return false; // Validate parameters
   }
-  
+
   // Create a RegisterBits object for the specific pin
-  Adafruit_BusIO_RegisterBits inputBit = Adafruit_BusIO_RegisterBits(input_port_reg, 1, pin);
-  
+  Adafruit_BusIO_RegisterBits inputBit =
+      Adafruit_BusIO_RegisterBits(input_port_reg, 1, pin);
+
   uint8_t bitValue;
   bitValue = inputBit.read();
-  
+
   return bitValue != 0; // Return true if bit is high, false if low
 }
--- Adafruit_XCA9554.h
+++ Adafruit_XCA9554.h
@@ -1,31 +1,32 @@
 #ifndef ADAFRUIT_XCA9554_H
 #define ADAFRUIT_XCA9554_H
 
-#include <Arduino.h>
-#include <Wire.h>
 #include "Adafruit_BusIO_Register.h"
 #include "Adafruit_I2CDevice.h"
+#include <Arduino.h>
+#include <Wire.h>
 
-#define XCA9554_REG_INPUT_PORT       0x00
-#define XCA9554_REG_OUTPUT_PORT      0x01
-#define XCA9554_REG_POLARITY_INVERT  0x02
-#define XCA9554_REG_CONFIG           0x03
+#define XCA9554_REG_INPUT_PORT 0x00
+#define XCA9554_REG_OUTPUT_PORT 0x01
+#define XCA9554_REG_POLARITY_INVERT 0x02
+#define XCA9554_REG_CONFIG 0x03
 
+/**! Class for adding generic pin control to xCA9554 GPIO expander */
 class Adafruit_XCA9554 {
 public:
-    Adafruit_XCA9554();
+  Adafruit_XCA9554();
 
-    bool begin(uint8_t i2c_addr, TwoWire *wire = &Wire);
-    bool pinMode(uint8_t pin, uint8_t mode);
-    bool digitalWrite(uint8_t pin, bool val);
-    bool digitalRead(uint8_t pin);
+  bool begin(uint8_t i2c_addr, TwoWire *wire = &Wire);
+  bool pinMode(uint8_t pin, uint8_t mode);
+  bool digitalWrite(uint8_t pin, bool val);
+  bool digitalRead(uint8_t pin);
 
 private:
-    Adafruit_I2CDevice *i2c_dev;
-    Adafruit_BusIO_Register *input_port_reg;
-    Adafruit_BusIO_Register *output_port_reg;
-    Adafruit_BusIO_Register *polarity_inversion_reg;
-    Adafruit_BusIO_Register *config_reg;
+  Adafruit_I2CDevice *i2c_dev;
+  Adafruit_BusIO_Register *input_port_reg;
+  Adafruit_BusIO_Register *output_port_reg;
+  Adafruit_BusIO_Register *polarity_inversion_reg;
+  Adafruit_BusIO_Register *config_reg;
 };
 
 #endif // ADAFRUIT_XCA9554_H
